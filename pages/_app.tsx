ddca232fd5b150b2309c822460d24a74f99ca863
Gaurav Bhandari <sbhandari1907@protonmail.com>
2022-12-24 09:43:47 +0530

    modified loader tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/HomePage/navbar/Navbar";
 import Footer from "../components/footer/Footer";
-import { AnimatePresence, AnimateSharedLayout, motion } from "framer-motion";
-import { useState } from "react";
-import Loader from "../components/loaders/Loader";
 export default function App({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(true);
-
   return (
     <>
       <main className="bg-hsBlack overflow-hidden">
