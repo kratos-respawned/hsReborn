b848498d9594768c666af159947f7d5bb77536f8
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-02-19 11:33:55 +0530

    static export

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["avatars.dicebear.com", "raw.githubusercontent.com"],
+    unoptimized: true,
   },
 };
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html className="bg-hsBlack" lang="en">
       <Head>
         <link rel="icon" href="/favicon.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
